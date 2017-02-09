1fe96049fc9c35d82c2741038c565d5ca9c8cafe
demyashev <demyashev@express.ru>
2017-02-09 20:19:29 +0300

    full page + modal

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -14,7 +14,6 @@ $(document).ready(function() {
             var slides = $('.lslide');
             var next = $(slides[1]).children('img').attr('alt');
             var prev = $(slides[$(slides).length - 1]).children('img').attr('alt');
-            console.log(prev);
 
             $('.lSAction').wrap('<div class="container"></div>');
             $('.lSAction').append('<span class="lSPrev-desc">' + prev + '</span>');
@@ -33,20 +32,115 @@ $(document).ready(function() {
         }
     });
 
+    
     $('.nav a').click(function(event) {
         var delta = 90; // body top padding
         var id = $(this).attr('href');
 
-        $('html, body').animate({
-            scrollTop: $(id).offset().top - delta
-        }, 2000);
+        if (id != '#') {
 
-        $('.nav a').each(function(index, el) {
-            $(el).removeClass('active');   
-        });
+            $('html, body').animate({
+                scrollTop: $(id).offset().top - delta
+            }, 2000, function(){
+                $('.nav a').parent('li').each(function(index, el) {
+                    $(el).removeClass('active');   
+                }); 
+            });
 
-        $(this).addClass('active');
-
-        return false;
+            $(this).parent('li').addClass('active');
+            
+            return false;
+        }
     });
+    
+    $("#signinForm").validate({
+        submitHandler: function(form){ 
+            
+            data = $(form).serialize();
+
+            console.log(data);
+
+            // $(form).submit();
+        },
+        errorClass: "error help-block",
+        validClass: "success",
+        errorElement: "p",
+        errorPlacement: function(error, element) {
+            element.parent(".form-group").addClass('has-error').children('label').after(error);
+        },
+        rules: {
+            username: {
+                required: true,
+                minlength: 3
+            },
+            email: {
+                required: true,
+                email: true,
+                minlength: 6
+            }
+        },
+        messages: {
+            username: {
+                required: "Поле обязательно для заполнения",
+                minlength: jQuery.validator.format("Длина значения должна быть не менее {0} символов")
+            },
+            email: {
+                required: "Поле обязательно для заполнения",
+                email: "Неверный формат email адреса",
+                minlength: jQuery.validator.format("Длина значения должна быть не менее {0} символов")
+            }
+        
+        }
+    });
+
+    $("#signinPhone").intlTelInput({
+        preferredCountries: ['ru', 'ua'],
+        initialCountry: "auto",
+        nationalMode: false,
+        formatOnDisplay: true,
+        initialCountry: 'ru',
+        geoIpLookup: function(callback) {
+            $.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+                var countryCode = (resp && resp.country) ? resp.country : "";
+                callback(countryCode);
+            });
+        },
+        utilsScript: "/public/js/utils.js" // just for formatting/placeholders etc
+    });
+
+    $("#signinPhone").on("keyup change", function() {
+        var intlNumber = $("#signinPhone").intlTelInput("getNumber");
+        if (intlNumber) {
+            console.log("International: " + intlNumber);
+        } else {
+            console.log("Please enter a number below");
+        }
+    });
+
+
+    var reset = function() {
+        $("#signinPhone").removeClass("error");
+        $('#signinForm .form-phone').removeClass("has-error");
+    };
+
+    // on blur: validate
+    $("#signinPhone").blur(function() {
+        reset();
+        if ($.trim($("#signinPhone").val())) {
+            if ($("#signinPhone").intlTelInput("isValidNumber")) {
+                // $('#signinForm .form-phone').find('p').remove();
+            } else {
+                console.log('qweqe');   
+                $('#signinForm .form-phone').addClass("has-error");
+                $('#signinForm .form-phone').children('p').html('Неверный формат').show();
+            }
+        }
+    });
+
+    // on keyup / change flag: reset
+    $("#signinPhone").on("keyup change", reset);
+
+
+    
+
 });
